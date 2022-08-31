d016850e0a60836b0482e5e2a234a43ef889caf2
henriqueholz <henriqueholz@live.com>
2022-08-31 19:29:41 -0300

    Creating header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { ThemeProvider } from "styled-components"
+import { Transactions } from "./pages/Transactions"
 import { defaultTheme } from "./styles/themes/default"
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <p>
-        Hello World
-      </p>
+      <Transactions />
     </ThemeProvider>
   )
 }
