0405e3aceb8b0b70c0f27c9ff2d84768f0eaf27f
henriqueholz <henriqueholz@live.com>
2022-09-01 10:12:40 -0300

    Addin radio button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog';
 
-import logoImg from   "../../assets/logo.svg"
-import { NewTransactionModal } from "../NewTransactionModel";
+import { AppleLogo, ArrowCircleUp, X } from "phosphor-react";
+
+// import logoImg from   "../../assets/logo.svg"
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoImg} alt="" />
+        <AppleLogo />
 
         <Dialog.Root>
         <Dialog.Trigger asChild>
